55d7038796dfed95b6cd49b369967ad4599bf591
Pedro Rodrigues <peddroprp@gmail.com>
2024-10-21 02:11:02 -0300

    add payment price to pay endpoint

--- parking-api/app/controllers/parking_controller.rb
+++ parking-api/app/controllers/parking_controller.rb
@@ -25,9 +25,12 @@ class ParkingController < ApplicationController
         render json: {message: "O pagamento referente a reserva #{parking.id} já foi realizado"}, status: :unprocessable_entity
       else
         elapsed_time = calculate_elapsed_time(parking)
+        minute_rate = 0.30
+        payment_price = calculate_payment_price(elapsed_time, minute_rate) 
+
         parking.update(paid: true, elapsed_time: elapsed_time) 
 
-        render json: {elapsed_time: elapsed_time, message: "Pagamento referente a reserva #{parking.id} realizado com sucesso"}, status: :ok
+        render json: {elapsed_time: elapsed_time, payment_price: payment_price, message: "Pagamento referente a reserva #{parking.id} realizado com sucesso"}, status: :ok
       end
     rescue ActiveRecord::RecordNotFound
       render json: {error: "reserva não encontrada"}, status: :not_found
@@ -46,6 +49,18 @@ class ParkingController < ApplicationController
     elapsed_time = (elapsed_time / 60).to_i
 
     return elapsed_time
+  end
+
+  # Calcula o valor do pagamento de acordo com o tempo de permanência
+  def calculate_payment_price(elapsed_time, minute_rate)
+    case elapsed_time
+    when 0..15
+      return 10.00
+    when 16..59
+      return elapsed_time * minute_rate
+    else
+      return elapsed_time * (minute/2)
+    end
   end
 
 end
