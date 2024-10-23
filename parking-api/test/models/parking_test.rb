72a16f34dc6fa388229bb7cda16c6d8dfd8afc33
Pedro Rodrigues <pedro.rodrigues@tradexsolutions.com.br>
2024-10-23 13:54:44 -0300

    Create user model tests

--- parking-api/test/models/parking_test.rb
+++ parking-api/test/models/parking_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class ParkingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without username" do
+    user = User.new(password: "testpass")
+    
+    assert_not user.save, "Saved the user without a username"
+  end
+
+  test "should save valid user" do
+    user = User.new(username: "testuser", password: "testpass")
+    
+    assert user.save, "Could not save a valid user"
+  end
 end
